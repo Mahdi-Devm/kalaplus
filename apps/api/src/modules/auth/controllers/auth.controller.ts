c7818662c79b696e27494dc7b50e1964f4c505a1
Mahdi-Devm <mahdibagherichanel@gmail.com>
2026-08-23 12:12:10 +0330

    feat(auth): add auth controller endpoints

--- apps/api/src/modules/auth/controllers/auth.controller.ts
+++ apps/api/src/modules/auth/controllers/auth.controller.ts
@@ -1,42 +1,27 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { CreateAuthDto } from '../dto/create-auth.dto';
-import { UpdateAuthDto } from '../dto/update-auth.dto';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('request-otp')
+  ReqOtp() {
+    return this.authService.ReqOtp();
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Post('vrify-otp')
+  VrifyOtp() {
+    return this.authService.VrifyOtp();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @Post('refresh')
+  Refresh() {
+    return this.authService.Refresh();
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('summary-user')
+  SummaryUser() {
+    return this.authService.SummaryUser();
   }
 }

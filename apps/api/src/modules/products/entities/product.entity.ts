b98ed87971e29510af55a9a5465a99bc4e4100e9
Mahdi-Devm <mahdibagherichanel@gmail.com>
2026-08-24 15:00:00 +0330

    refactor(product):refactor entity for create a full product

--- apps/api/src/modules/products/entities/product.entity.ts
+++ apps/api/src/modules/products/entities/product.entity.ts
@@ -5,6 +5,27 @@ import { Category } from './category.entity';
 @Entity()
 export class Product extends BaseEntity {
   @Column()
+  title: string;
+
+  @Column()
+  shortDescription: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column()
+  mainImage: string;
+
+  @Column('text', { array: true })
+  images: string[];
+
+  @Column({ type: 'float', nullable: true })
   discountPercent?: number;
 
   @Column({ default: null })

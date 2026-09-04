4572a80c2749ac4230aca4cb522db4358338fd92
Mahdi-Devm <mahdibagherichanel@gmail.com>
2026-09-04 07:18:28 +0330

    refactor(buttom):refactor button shadcn style

--- apps/web/src/core/components/shadcn/ui/button/button.tsx
+++ apps/web/src/core/components/shadcn/ui/button/button.tsx
@@ -4,45 +4,40 @@ import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[5px] text-sm   transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none  shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-ring/50 focus-visible:ring-offset-2",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm   transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none  shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-ring/50 focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
         default:
           "bg-primary text-primary-foreground shadow-sm " +
           "hover:bg-primary-dark hover:shadow-sm hover:scale-[1.01] " +
-          "active:scale-95 active:shadow-sm " +
-          "transform transition-all duration-200",
+          "active:scale-95 active:shadow-sm ",
 
         secondary:
-          "bg-secondary text-background shadow-sm " +
-          "hover:bg-secondary/90 hover:shadow-sm hover:scale-[1.01] " +
-          "active:scale-95 active:shadow-sm " +
-          "transform transition-all duration-200",
+          "bg-secondary text-black shadow-sm " +
+          "hover:bg-secondary-foreground/90 hover:shadow-sm hover:scale-[1.01] " +
+          "active:scale-95 active:shadow-sm ",
 
         outline:
           "border-1 border-primary text-primary bg-transparent " +
           "hover:bg-primary/10 hover:scale-[1.01] hover:shadow-sm " +
-          "active:scale-95 " +
-          "transform transition-all duration-200",
+          "active:scale-95 ",
 
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm " +
           "hover:bg-destructive/90 hover:scale-[1.01] hover:shadow-sm " +
-          "active:scale-95 " +
-          "transform transition-all duration-200",
+          "active:scale-95 ",
 
         ghost:
           "bg-white/10 backdrop-blur-sm text-foreground " +
           "hover:bg-white/20 hover:scale-[1.01] " +
-          "active:scale-95 " +
-          "transform transition-all duration-200",
+          "active:scale-95 ",
 
         link:
           "text-primary underline-offset-4 hover:underline " +
           "hover:scale-[1.01] " +
           "active:scale-95 " +
-          "transform transition-all duration-200 p-0",
+          "  p-0",
       },
       size: {
         default: "h-10 px-6 py-2 text-base",

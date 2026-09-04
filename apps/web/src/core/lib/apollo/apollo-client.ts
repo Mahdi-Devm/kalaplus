b416e1e3f15c035e9b6a643a544909c08a7bc6e5
Mahdi-Devm <mahdibagherichanel@gmail.com>
2026-09-04 10:23:14 +0330

    refactor(ApolloClient):add credentials for token body

--- apps/web/src/core/lib/apollo/apollo-client.ts
+++ apps/web/src/core/lib/apollo/apollo-client.ts
@@ -5,6 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
     uri: "http://localhost:4000/graphql",
+    credentials: "include",
   }),
   cache: new InMemoryCache(),
 });

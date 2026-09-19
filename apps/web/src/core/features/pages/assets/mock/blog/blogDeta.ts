179581b5f2979be7d531212dfafc487e3fc3e3e7
niyayesh <niyayeshammardii@gmail.com>
2026-09-19 04:58:39 -0700

    edit data fake in blog

---
+++ apps/web/src/core/features/pages/assets/mock/blog/blogDeta.ts
@@ -0,0 +1,24 @@
+const BlogData = [
+  {
+    id: "1",
+    title: "راهنمای کامل بررسی کفش فوتبال نایک برای خرید",
+    img: "/common/img/blog/blog2.png",
+  },
+  {
+    id: "2",
+    title: "راهنمای خرید کیت رئال مادرید 2024",
+    img: "/common/img/blog/blog3.png",
+  },
+  {
+    id: "3",
+    title: "بررسی کفش فوتسال نایک",
+    img: "/common/img/blog/blog2.png",
+  },
+  {
+    id: "4",
+    title: "۵ اشتباه رایج هنگام خرید که بهتر است انجام ندهید",
+    img: "/common/img/blog/blog3.png",
+  },
+];
+
+export default BlogData;
